77ee59d78509b8dfec2a7cd18b32cf6334e8cbb3
Christian Damete-Yeboa <christiandameteyeboa@gmail.com>
2024-12-10 16:55:33 +0100

    fix/frontend-CommonService/update Common Service

--- frontend/src/services/CommonService.tsx
+++ frontend/src/services/CommonService.tsx
@@ -1,7 +1,9 @@
+import { env } from "process";
+
 const EXTERNAL_DOMAIN: string = "https://clothing-store-ecommerce-platform-backend.onrender.com";
 
 // Check if the app is running in production or development
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = env.NODE_ENV === "production";
 
 // Use the external domain in production, otherwise use localhost
 export const API_BASE_DOMAIN: string = isProduction ? EXTERNAL_DOMAIN : "http://localhost:8000";
