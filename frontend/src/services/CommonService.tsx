6f20ee7567c697efd7ab4b90366d0a0c275caafd
Christian Damete-Yeboa <christiandameteyeboa@gmail.com>
2024-12-10 18:48:04 +0100

    misc/

--- frontend/src/services/CommonService.tsx
+++ frontend/src/services/CommonService.tsx
@@ -1,6 +1,5 @@
-const EXTERNAL_DOMAIN: string = "https://clothing-store-ecommerce-platform-backend.onrender.com";
-
-// Check if the app is running in production or development
 const isProduction = window.location.hostname !== "localhost";
-// Use the external domain in production, otherwise use localhost
-export const API_BASE_DOMAIN: string = isProduction ? EXTERNAL_DOMAIN : "http://localhost:8000";
+
+export const API_BASE_DOMAIN: string = isProduction
+  ? "https://clothing-store-ecommerce-platform-backend.onrender.com"
+  : "http://localhost:8000";
