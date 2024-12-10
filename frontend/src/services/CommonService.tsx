ffddb1233ca9d10099218f4d18e92a8cf43b9019
Christian Damete-Yeboa <christiandameteyeboa@gmail.com>
2024-12-10 18:56:25 +0100

    revert

--- frontend/src/services/CommonService.tsx
+++ frontend/src/services/CommonService.tsx
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 const isProduction = window.location.hostname !== "localhost";
 
 export const API_BASE_DOMAIN: string = isProduction
   ? "https://clothing-store-ecommerce-platform-backend.onrender.com"
   : "http://localhost:8000";
+=======
+export const API_BASE_DOMAIN: string = "http://localhost:8000";
+>>>>>>> parent of a9910dc (fix/frontend-CommonService/updated environment variables)
