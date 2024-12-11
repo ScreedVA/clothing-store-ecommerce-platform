8bc7c972d279eae6a601f6e4f39dcc276c680641
Christian Damete-Yeboa <christiandameteyeboa@gmail.com>
2024-12-11 10:51:39 +0100

    misc/backend/update config #6

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",   
+    "https://clothing-store-ecommerce-platform.onrender.com/"
 ]
 
 app.add_middleware(
